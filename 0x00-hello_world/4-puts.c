d9ebc827c190bfdc720f8062fbd33716daa7b1c7
eljadydy <eljadydy@usmba.ma>
2023-03-01 02:10:19 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main : entry function
+ * return is a final success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
