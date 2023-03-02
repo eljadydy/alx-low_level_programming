1fd571787f7ff83846e1bbd7aab374070907092b
eljadydy <eljadydy@usmba.ma>
2023-03-02 01:41:20 -0800

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,7 +4,7 @@
  *main -> assign a random number to the variable n each time it is executed
  *and print the last digit of the number stored in the variable n
  *Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	char ch;
@@ -16,8 +16,6 @@ int main(void)
 			putchar(ch);
 		}
 	}
-
 	putchar(10); /* this is an ascii code for new line*/
-
 	return (0);
 }
