980c8525ff3f4c2782814fdca7cd4773299cb59f
eljadydy <eljadydy@usmba.ma>
2023-03-02 08:29:33 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,28 @@
 #include "holberton.h"
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * rot13 - change letters to ROT13.
+ * @s: analized string.
+ *
+ * Return: String with all letters in ROT13 base.
  */
-void print_number(int n)
+char *rot13(char *s)
 {
-	unsigned int b;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j;
 
-	if (n < 0)
+	while (*(s + i) != '\0')
 	{
-		b = -n;
-		_putchar('-');
+		for (j = 0; j <= 51; j++)
+		{
+			if (*(s + i) == a[j])
+			{
+				*(s + i) = rot[j];
+				break;
+			}
+		}
+		i++;
 	}
-	else
-	{
-		b = n;
-	}
-	if (b / 10)
-	{
-		print_number(b / 10);
-	}
-	_putchar((b % 10) + '0');
+	return (s);
 }
